9439b96fcd431a74e95ed63ece639d81372d0529
E-R-C <ehuynh.school@gmailcom>
2018-02-25 16:44:22 -0600

    Setup up to before scaffolding

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using DDSWebstore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DDSWebstore
 {
@@ -21,6 +23,8 @@ namespace DDSWebstore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ItemContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
             services.AddMvc();
         }
 
