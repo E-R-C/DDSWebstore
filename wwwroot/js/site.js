9c453f8447ad7c5b93d584b848c28c34f86e5a92
annaholmes <holmesaa@hendrix.edu>
2018-03-28 23:40:28 -0500

    main page fixin + linked css and js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,15 @@
-﻿// Write your Javascript code.
+﻿$(document).ready(function () {
+    $('.categories tr').click(function (event) {
+        if (event.target.type !== 'checkbox') {
+            $(':checkbox', this).trigger('click');
+        }
+    });
+
+    $("input[type='checkbox']").change(function (e) {
+        if ($(this).is(":checked")) {
+            $(this).closest('tr').addClass("highlight_row");
+        } else {
+            $(this).closest('tr').removeClass("highlight_row");
+        }
+    });
+});
