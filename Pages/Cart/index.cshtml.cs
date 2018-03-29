282feb23aec1cedc7daf8c8e19eeed627d13d449
E-R-C <ehuynh.school@gmailcom>
2018-03-28 23:38:40 -0500

    sort of fixed links and added cart

---
+++ Pages/Cart/index.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DDSWebstore.Models;
+
+namespace DDSWebstore.Pages.Cart
+{
+    public class IndexModel : PageModel{
+        private readonly DDSWebstore.Models.MyDBContext _context;
+
+        public IndexModel(DDSWebstore.Models.MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Item> Item { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Item = await _context.Item.ToListAsync();
+        }
+    }
+}
