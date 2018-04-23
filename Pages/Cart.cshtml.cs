46fe1581668651fe0209e39999763702f4f3caa3
Bosco Ndemeye <ndemeyemm@hendrix.edu>
2018-04-23 11:33:24 -0500

    Merge Pages/Cart.cshtml

--- Pages/Cart.cshtml.cs
+++ Pages/Cart.cshtml.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DDSWebstore.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace DDSWebstore.Pages
 {
     public class CartModel : PageModel{
         private readonly DDSWebstore.Models.MyDBContext _context;
+        // private CookieOptions ddsCookie;
 
         public CartModel(DDSWebstore.Models.MyDBContext context)
         {
@@ -19,8 +21,52 @@ namespace DDSWebstore.Pages
 
         public IList<Item> Item { get;set; }
         public async Task OnGetAsync()
-        {
+        {   
             Item = await _context.Item.ToListAsync();
-        }
+            cookieContents = Request.Cookies["ddsCookie"];
+            IList<Item> returned = new List<Item>();
+            foreach(int id in cookieContents){
+                var item = _context.Item.Single(i => i.ID == id);
+                returned.Add(item);
+            }
+            Item = await returned.ToListAsync();
+
     }
 }
+
+
+// using System.Linq;
+// using ViewInjectSample.Interfaces;
+
+// namespace ViewInjectSample.Model.Services
+// {
+//     public class StatisticsService
+//     {
+//         private readonly IToDoItemRepository _toDoItemRepository;
+
+//         public StatisticsService(IToDoItemRepository toDoItemRepository)
+//         {
+//             _toDoItemRepository = toDoItemRepository;
+//         }
+
+//         public int GetCount()
+//         {
+//             return _toDoItemRepository.List().Count();
+//         }
+
+//         public int GetCompletedCount()
+//         {
+//             return _toDoItemRepository.List().Count(x => x.IsDone);
+//         }
+
+//         public double GetAveragePriority()
+//         {
+//             if (_toDoItemRepository.List().Count() == 0)
+//             {
+//                 return 0.0;
+//             }
+
+//             return _toDoItemRepository.List().Average(x => x.Priority);
+//         }
+//     }
+// }
